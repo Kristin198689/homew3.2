0e77d56aa93a4792a86a1d069eed4dccf67c599a
Kristin198689 <163996836+Kristin198689@users.noreply.github.com>
2024-08-09 10:46:19 +0200

    fertig

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import List from './List';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Приложение с рейтингом и списком приглашенных</h1>
+      
+      <List />
+    </div>
+  );
+};
+
+export default App;
+
